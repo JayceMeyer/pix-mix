631be4904936c7d7fedc6f3038d6084acbdaa762
Jayce Meyer <jnmeyer@nps.gov>
2024-10-07 15:07:15 -0600

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import { ImageCard } from './components/ImageCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
